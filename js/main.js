d4a14600c599863f617ec6826271de8bcad6672c
webdev20071995 <sharofiddinovam6@gmail.com>
2022-09-20 19:13:22 +0500

    add pages text and images

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const elFooterLinkColorInsta = document.querySelector('.footer__link-color-insta');
+const elFooterLinkColorTele = document.querySelector('.footer__link-color-tele');
+const elFooterLinkColorFace = document.querySelector('.footer__link-color-face');
+const elFooterLinkColorEamil = document.querySelector('.footer__link-color-eamil');
+
+
+// elFooterLinkColorInsta(function ('click', elFooterLinkColorTele));
